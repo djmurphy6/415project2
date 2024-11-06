44c0aa63d32db2ab2cfbf308ae4e58f857d7b529
djmurphy6 <dmurphy6@uoregon.edu>
2024-11-06 15:36:20 -0800

    making progress on part1

--- part1.c
+++ part1.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include <sys/types.h>
 #include<stdlib.h>
+#include <string.h>
 #include<unistd.h>
 #include<sys/wait.h>
+#include "string_parser.h"
 
 void script_print (pid_t* pid_ary, int size);
 
@@ -13,33 +15,80 @@ int main(int argc,char*argv[])
 		printf ("Wrong number of arguments\n");
 		exit (0);
 	}
+
+	 //opening file to read
+    FILE *inFPtr;
+    inFPtr = fopen (argv[1], "r");
+    if(inFPtr == NULL) {
+        printf("Error opening file");
+        return 1;
+    }
+
+    //declare line_buffer
+    size_t len = 128;
+    char* line_buf = malloc (len);
+    if (line_buf == NULL) {
+        printf("Error allocating memory for line_buf");
+        fclose(inFPtr); // Close the file 
+        return 1;
+    }
+
+    command_line large_token_buffer;
+
+
+    int line_num = 0;
+
+    //loop until the file is over
+    while (getline(&line_buf, &len, inFPtr) != -1)
+    {
+        //tokenize line buffer
+        //large token is seperated by ";"
+        large_token_buffer = str_filler (line_buf, "\n");
+        //iterate through each large token
+        for (int i = 0; large_token_buffer.command_list[i] != NULL; i++)
+        {
+                    
+            printf("processing command: %s\n", large_token_buffer.command_list[i]);
+
+        }
+
+        //free smaller tokens and reset variable
+        free_command_line (&large_token_buffer);
+        //memset (&large_token_buffer, 0, 0);
+    }
+
+    fclose(inFPtr);
+    //free line buffer
+    free (line_buf);
+    printf("End of file\nBye Bye\n");
+
 	int n = atol(argv[1]);
 
-	pid_t *child_processes = malloc(n*sizeof(pid_t));
+	pid_t *child_processes = malloc(n*sizeof(pid_t));	// declare child process pool
 
 	
-
+	// spawn n new peocesses
 	for(int i=0; i<n; i++){
 		child_processes[i] = fork();
+		if (child_processes[i]<0) {
+			// error handling
+		}
+		if (child_processes[i] == 0) {
+			// if (execvp(path, arg) == -1) {
+				// error handling
+			// }
+
+			//
+			exit(-1);
+		}
 		
 		char *args[] = {".iobound", "-seconds", "15", 0};
 	}
 
-	script_print(child_processes, n);
+	script_print(child_processes, n);	// call script_print
 
-
-	/*
-	*	TODO
-	*	#1	declare child process pool
-	*	#2 	spawn n new processes
-	*		first create the argument needed for the processes
-	*		for example "./iobound -seconds 10"
-	*	#3	call script_print
-	*	#4	wait for children processes to finish
-	*	#5	free any dynamic memory
-	*/
-	while(wait(NULL) > 0);
-	free(child_processes);
+	while(wait(NULL) > 0);	// wait for children process to finish
+	free(child_processes);	// free mem
 	return 0;
 }
 
