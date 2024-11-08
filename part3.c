2cc58b17bf6c1a1af6d8c6e3c959aa5ba623fa1e
djmurphy6 <dmurphy6@uoregon.edu>
2024-11-08 12:04:27 -0800

    fix part3 issue

--- part3.c
+++ part3.c
@@ -62,7 +62,6 @@ int main(int argc, char* argv[]) {
                 perror("execvp failed");
                 free_command_line(&cmd);
                 free(line_buf);
-                fclose(inFPtr);
                 exit(1);
             }
         } else {
