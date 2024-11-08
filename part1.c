faf392b9081e5c5441a943f428c49e3524b9e590
djmurphy6 <dmurphy6@uoregon.edu>
2024-11-08 11:31:35 -0800

    memory leak

--- part1.c
+++ part1.c
@@ -6,8 +6,6 @@
 #include<sys/wait.h>
 #include "string_parser.h"
 
-void script_print (pid_t* pid_ary, int size);
-
 int main(int argc,char*argv[])
 {
 	if (argc != 2)
@@ -80,9 +78,6 @@ int main(int argc,char*argv[])
         free_command_line(&cmd);
     }
 
-	// Print the top script to monitor child processes
-    script_print(pid_ary, pid_count);
-
     // Wait for all child processes to finish
     for (int i = 0; i < pid_count; i++)
     {
@@ -98,34 +93,3 @@ int main(int argc,char*argv[])
 
     return 0;
 }
-
-
-void script_print (pid_t* pid_ary, int size)
-{
-	FILE* fout;
-	fout = fopen ("top_script.sh", "w");
-	fprintf(fout, "#!/bin/bash\ntop");
-	for (int i = 0; i < size; i++)
-	{
-		fprintf(fout, " -p %d", (int)(pid_ary[i]));
-	}
-	fprintf(fout, "\n");
-	fclose (fout);
-
-	char* top_arg[] = {"gnome-terminal", "--", "bash", "top_script.sh", NULL};
-	pid_t top_pid;
-
-	top_pid = fork();
-	{
-		if (top_pid == 0)
-		{
-			if(execvp(top_arg[0], top_arg) == -1)
-			{
-				perror ("top command: ");
-			}
-			exit(0);
-		}
-	}
-}
-
-
